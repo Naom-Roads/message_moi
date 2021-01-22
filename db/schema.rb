ffc2cad45cd1148b1a07f9f6334e7c724337d8d9
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2021-01-22 12:40:06 +0100

    Added user and model table and validations

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,7 @@ ActiveRecord::Schema.define(version: 2021_01_19_133612) do
 
   create_table "users", force: :cascade do |t|
     t.string "username"
+    t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
