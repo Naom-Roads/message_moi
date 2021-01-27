3f6c365a6683d2828402b72236aa7ea71d8b182a
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2021-01-27 15:09:37 +0100

    Added Login Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :users
   root 'chatroom#index'
   get 'login', to: 'users#new'
+  post 'login', to: "users#create"
+  delete 'logout', to: 'users#destroy'
 
 end
