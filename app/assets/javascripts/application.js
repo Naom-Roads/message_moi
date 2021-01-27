25ff63699acaada8b6552d9c332d8564a2ac00d5
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2021-01-27 15:16:38 +0100

    Added Success and Error Messages for Logging in and out

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,4 +18,11 @@
 //= require_tree .
 $(document).on('turbolinks:load', function(){
     $('.ui.dropdown').dropdown();
+    $('.message .close')
+  .on('click', function() { 
+    $(this).closest('.message').transition('fade')
+    ;
+  })
+;
 })
+
