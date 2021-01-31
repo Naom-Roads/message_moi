422b94cdcd3973fbd072df789d1e802ffb0faafe
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2021-01-31 16:03:56 +0100

    postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2021_01_25_154145) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "messages", force: :cascade do |t|
     t.text "body"
     t.integer "user_id"
