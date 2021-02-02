40521862a27a506bad5765855a8bde5029263222
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2021-02-02 13:47:30 +0100

    Added Signup route

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   resources :users
+
   root 'chatroom#index'
+  get 'signup', to: 'users#new'
   get 'login', to: 'users#new'
   post 'login', to: "users#create"
   delete 'logout', to: 'users#destroy'
   post 'message', to: 'messages#create' 
-
  
 
   Rails.application.routes.draw do
