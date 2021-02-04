19a424bd8bacc83cde70fe103d626dff9fe383c1
Naoms88 <44932790+Naoms88@users.noreply.github.com>
2021-02-04 14:04:26 +0100

    Removed all updates for Multiple Channels and rolledback to previous state just after redis installation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,7 +15,7 @@
 //= require activestorage
 //= require turbolinks
 //= require semantic-ui
-//= require_tree ./channels
+//= require_tree .
 
 scroll_bottom = function() {
     if ($('#messages').length > 0) {
